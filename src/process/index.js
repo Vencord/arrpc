95bcc4ab9e66812d9b112ec8754079c67d6613f5
Vendicated <vendicated@riseup.net>
2026-09-08 01:49:46 +0200

    process: pull detectable database at runtime
    Co-Authored-By: Kylie C <kyliepc@proton.me>

--- src/process/index.js
+++ src/process/index.js
@@ -6,11 +6,30 @@ import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const DetectableDB = JSON.parse(fs.readFileSync(join(__dirname, 'detectable.json'), 'utf8'));
+const databasePath = process.env.ARRPC_DETECTABLE_CACHE_PATH || join(__dirname, 'detectable.json');
 
 import * as Natives from './native/index.js';
 const Native = Natives[process.platform];
 
+async function getDatabase(lastModified) {
+  const res = await fetch("https://discord.com/api/v9/applications/detectable", {
+    method: 'GET',
+    headers: lastModified ? { 'If-Modified-Since': lastModified } : {}
+  })
+
+  if (res.status === 304) return false;
+  if (res.status !== 200) throw new Error(`http code ${res.status}`);
+
+  const data = await res.text();
+
+  try {
+    await fs.promises.writeFile(databasePath, JSON.stringify(JSON.parse(data)), 'utf8');
+    return true;
+  }
+  catch (err) {
+    throw new Error(`failed retrieving the database: ${err}`);
+  }
+}
 
 const timestamps = {}, names = {}, pids = {};
 export default class ProcessServer {
@@ -18,13 +37,40 @@ export default class ProcessServer {
     if (!Native) return; // log('unsupported platform:', process.platform);
 
     this.handlers = handlers;
+    this.DetectableDB = null;
 
     this.scan = this.scan.bind(this);
 
-    this.scan();
-    setInterval(this.scan, 5000);
+    this.initializeDatabase().then(() => {
+      this.scan();
+      setInterval(this.scan, 5000);
+      log('started');
+    });
 
-    log('started');
+  }
+  async initializeDatabase() {
+    log("initializing database")
+    const stats = await fs.promises.stat(databasePath).catch(() => null);
+    if (stats && Date.now() - stats.mtime.getTime() < 24 * 60 * 60 * 1000) {
+      log('database is up to date');
+      this.DetectableDB = JSON.parse(await fs.promises.readFile(databasePath, "utf-8"));
+      return;
+    }
+
+    const age = stats?.mtime.toUTCString() ?? null;
+
+    await getDatabase(age)
+      .then(updated => {
+        if (updated) log('database updated successfully')
+      })
+      .catch(error => { log(`${error}.. continuing with old database`) });
+
+    try {
+      this.DetectableDB = JSON.parse(await fs.promises.readFile(databasePath, "utf-8"));
+    } catch (err) {
+      fs.promises.unlink(databasePath).catch(() => { });
+      throw new Error(`could not load the database. aborting... ${err}`)
+    }
   }
 
   async scan() {
@@ -52,7 +98,7 @@ export default class ProcessServer {
         toCompare.push(p.replace('_64', ''));
       }
 
-      for (const { executables, id, name } of DetectableDB) {
+      for (const { executables, id, name } of this.DetectableDB) {
         if (executables?.some(x => {
           if (x.is_launcher) return false;
           if (x.name[0] === '>' ? x.name.substring(1) !== toCompare[0] : !toCompare.some(y => x.name === y)) return false;
